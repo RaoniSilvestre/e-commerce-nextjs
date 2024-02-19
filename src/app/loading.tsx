b7f7b5911e545d464923800e5a41e6ba372559f7
Raoni <raoni.silvestre@hotmail.com>
2024-02-19 20:28:20 -0300

    Add LoadingPage component

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function LoadingPage() {
+  return (
+    <div className="h-screen flex items-center justify-center ">
+      <span className="loading loading-dots loading-lg m-auto block" />
+    </div>
+  )
+}
