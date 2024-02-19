5a4b2a8ef3f04e2769b9a750d0882b625df4c4a4
Raoni <raoni.silvestre@hotmail.com>
2024-02-19 20:27:54 -0300

    Update tailwind.config.ts with new theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,15 +11,18 @@ const config: Config = {
     themes: [
       {
         mytheme: {
-          "primary": "#9333ea",
-          "secondary": "#22d3ee",
-          "accent": "#a5f3fc",
-          "neutral": "#130208",
+          primary: "#9333ea",
+          secondary: "#22d3ee",
+          accent: "#a5f3fc",
+          neutral: "#130208",
           "base-100": "#fff2ea",
-          "info": "#00e1ff",
-          "success": "#009f5c",
-          "warning": "#e9be00",
-          "error": "#d1003d",
+          info: "#00e1ff",
+          success: "#009f5c",
+          warning: "#e9be00",
+          error: "#d1003d",
+          body: {
+            "background-color": "#f9fafb"
+          }
         },
       },
     ],
