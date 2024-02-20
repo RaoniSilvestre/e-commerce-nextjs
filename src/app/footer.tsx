083a59dc11c7f6cb8a8164fb0c58a27163841aeb
Raoni <raoni.silvestre@hotmail.com>
2024-02-19 23:51:30 -0300

    Add Footer component

---
+++ src/app/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <aside>
+        <p>Copyright © 2024 - Todos os direitos reservados por Lalavi Biju</p>
+      </aside>
+    </footer>
+  )
+}
