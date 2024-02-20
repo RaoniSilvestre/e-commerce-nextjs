77e9c8f3c5f597f4bd97e760943ad2a8180b828f
Raoni <raoni.silvestre@hotmail.com>
2024-02-19 23:51:53 -0300

    Add Navbar component with search functionality and shopping cart button

---
+++ src/app/Navbar/navbar.tsx
@@ -0,0 +1,52 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import logo from '../../assets/logo.png'
+import ShoppingCartButton from './shopping-cart-button'
+import { getCart } from '@/lib/db/cart'
+import { redirect } from 'next/navigation'
+
+async function searchProducts(formData: FormData){
+  "use server"
+
+  const searchQuery = formData.get('searchQuery')?.toString()
+
+  if(searchProducts){
+    redirect("/search?query=" + searchQuery)
+  }
+}
+
+
+export default async function Navbar() {
+
+  const cart = await getCart()
+
+  return (
+    <div className="bg-neutral">
+
+      <div className="navbar max-w-7xl m-auto flex-col sm:flex-row gap-2">
+
+        <div className="flex-1">
+
+          <Link href="/" className="btn btn-ghost text-xl normal-case">
+            <Image src={logo} alt="LalaviBiju logo" width={40} height={40} />
+            Lalavi Biju
+          </Link>
+        </div>
+
+        <div className="flex-none gap-2">
+          <form action={searchProducts}>
+            <div className="form-control">
+              <input
+                name="searchQuery"
+                className="input input-bordered w-full min-w-[100px]"
+                placeholder="Pesquisar produtos"
+              />
+            </div>
+          </form>
+          <ShoppingCartButton cart={cart} />
+        </div>
+
+      </div>
+    </div>
+  )
+}
