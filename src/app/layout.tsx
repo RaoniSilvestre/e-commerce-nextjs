7ecd40e6b57b679278c2f936d016644adf2b5e93
Raoni <raoni.silvestre@hotmail.com>
2024-02-21 16:58:11 -0300

    Add SessionProvider component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './Navbar/navbar'
 import Footer from './footer'
+import SessionProvider from './session-provider'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -16,11 +18,13 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt">
       <body className={inter.className}>
-        <Navbar/>
-        <main className="p-4 max-w-7xl m-auto min-w-[300px]">{children}</main>
-        <Footer/>
+        <SessionProvider>
+          <Navbar />
+          <main className="p-4 max-w-7xl m-auto min-w-[300px]">{children}</main>
+          <Footer />
+        </SessionProvider>
       </body>
     </html>
   )
