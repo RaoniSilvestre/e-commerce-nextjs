10a4a74058ebf847a8a7debf18a4b3b2b159112b
Raoni <raoni.silvestre@hotmail.com>
2024-02-21 16:58:31 -0300

    Fix cart item quantity update logic

--- src/app/cart/actions.ts
+++ src/app/cart/actions.ts
@@ -1,5 +1,4 @@
-
-"use server";
+'use server'
 
 import { getCart, createCart } from '@/lib/db/cart'
 import { revalidatePath } from 'next/cache'
@@ -8,32 +7,44 @@ export async function setProductQuantity(productId: string, quantity: number) {
 
   const articleInCart = cart.items.find((item) => item.productId === productId)
 
-  if(quantity === 0 && articleInCart) {
-    await prisma.cartItem.delete({
-     where: {
-        id: articleInCart.id
-      }
-    })
-    
-  } else if( articleInCart) {
-    await prisma.cartItem.update({
-    where: {
-      id: articleInCart.id
-    },
-    data: {
-      quantity
+  if (quantity === 0) {
+    if (articleInCart) {
+      await prisma.cart.update({
+        where: { id: cart.id },
+        data: {
+          items: {
+            delete: { id: articleInCart.id },
+          },
+        },
+      })
     }
-    
-    })
   } else {
-    await prisma.cartItem.create({
-      data: {
-        cartId: cart.id,
-        productId,
-        quantity
-      }
-    })
+    if (articleInCart) {
+      await prisma.cart.update({
+        where: { id: cart.id },
+        data: {
+          items: {
+            update: {
+              where: { id: articleInCart.id },
+              data: { quantity },
+            },
+          },
+        },
+      })
+    } else {
+      await prisma.cart.update({
+        where: { id: cart.id },
+        data: {
+          items: {
+            create: {
+              productId,
+              quantity,
+            },
+          },
+        },
+      })
+    }
   }
 
-  revalidatePath("/cart", "page")
+  revalidatePath('/cart', 'page')
 }
