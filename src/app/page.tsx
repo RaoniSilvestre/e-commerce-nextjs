e60c27820f4243bd3e9d2118268368fd159e320b
Raoni <raoni.silvestre@hotmail.com>
2024-02-21 16:58:15 -0300

    Add pagination functionality to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,36 @@ import ProductCard from '../components/product-card'
 import { prisma } from '@/lib/db/prisma'
 import Image from 'next/image'
 import Link from 'next/link'
+import PaginationComponent from '@/components/pagination-bar'
 
-export default async function Home() {
+interface HomeProps {
+  searchParams: { page: string }
+}
+
+export default async function Home({
+  searchParams: { page = '1' },
+}: HomeProps) {
+  const currentPage = parseInt(page)
+
+  const pageSize = 6
+  const heroItemCount = 1
+
+  const totalItemCount = await prisma.product.count()
+  const totalPages = Math.ceil((totalItemCount - heroItemCount) / pageSize)
+
   const products = await prisma.product.findMany({
     orderBy: {
       id: 'desc',
     },
+    skip:
+      (currentPage - 1) * pageSize + (currentPage === 1 ? 0 : heroItemCount),
+    take: pageSize + (currentPage === 1 ? heroItemCount : 0),
   })
 
   return (
     <div>
-      <div className="hero rounded-xl bg-base-200">
+      {currentPage === 1 && (
+        <div className="hero rounded-xl bg-base-200">
         <div className="hero-content flex-col lg:flex-row">
           <Image
             src={products[0].imageUrl}
@@ -34,11 +53,16 @@ export default async function Home() {
           </div>
         </div>
       </div>
+      )}
 
       <div className="my-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {products.slice(1).map((product) => (
+        {(currentPage===1 ? products.slice(1): products).map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
+      </div>
+      <div className="flex justify-center">
+        {totalPages > 1 &&  <PaginationComponent currentPage={currentPage} totalPages={totalPages} />}
+        
       </div>
     </div>
   )
