ec4aa7575834f134333d5c2bdaabe69f9d40400c
Raoni <raoni.silvestre@hotmail.com>
2024-02-21 16:58:35 -0300

    Add UserMenuButton to Navbar

--- src/app/Navbar/navbar.tsx
+++ src/app/Navbar/navbar.tsx
@@ -2,31 +2,29 @@ import Link from 'next/link'
 import Image from 'next/image'
 import logo from '../../assets/logo.png'
 import ShoppingCartButton from './shopping-cart-button'
+import UserMenuButton from './user-menu-button'
 import { getCart } from '@/lib/db/cart'
 import { redirect } from 'next/navigation'
-
-async function searchProducts(formData: FormData){
-  "use server"
-
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+async function searchProducts(formData: FormData) {
+  'use server'
+  
   const searchQuery = formData.get('searchQuery')?.toString()
 
-  if(searchProducts){
-    redirect("/search?query=" + searchQuery)
+  if (searchProducts) {
+    redirect('/search?query=' + searchQuery)
   }
 }
 
-
 export default async function Navbar() {
-
+  const session = await getServerSession(authOptions)
   const cart = await getCart()
 
   return (
     <div className="bg-neutral">
-
       <div className="navbar max-w-7xl m-auto flex-col sm:flex-row gap-2">
-
         <div className="flex-1">
-
           <Link href="/" className="btn btn-ghost text-xl normal-case">
             <Image src={logo} alt="LalaviBiju logo" width={40} height={40} />
             Lalavi Biju
@@ -44,8 +42,8 @@ export default async function Navbar() {
             </div>
           </form>
           <ShoppingCartButton cart={cart} />
+          <UserMenuButton session={session}/>
         </div>
-
       </div>
     </div>
   )
