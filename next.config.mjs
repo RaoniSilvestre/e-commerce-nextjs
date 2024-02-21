34be0d5c0acbfb66d33cf0188543e9bca6a8282d
Raoni <raoni.silvestre@hotmail.com>
2024-02-21 16:58:02 -0300

    Update remotePatterns in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,9 @@ const nextConfig = {
   images: {
     remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com',
     port: '',
-  }],
+  },
+  {protocol: 'https', hostname: 'lh3.googleusercontent.com', port: ''}
+],
   },
 }
 
